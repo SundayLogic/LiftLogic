ff402aa703e396b9a0be57ae2b99dbdf3a2b6fa6
Pablo <contactoemail.pablo@gmail.com>
2023-06-12 17:14:11 +0200

    Setting Up & Installing Dependencies

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className='flex justify-center items-center h-[100vh] w-[100vw] bg-black'>
+      <h1>LiftLogic</h1>
+    </div>
+  )
+}
+
+export default App
